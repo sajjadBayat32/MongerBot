ee8be3392ff4ee2ef92706a194a47ca61a67e777
sajjad <sajjad.bayat32@gmail.com>
2020-12-21 00:17:12 +0330

    the project updated to angular v11

--- src/app/strategy/strategy.component.spec.ts
+++ src/app/strategy/strategy.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StrategyComponent } from './strategy.component';
 
@@ -6,7 +6,7 @@ describe('StrategyComponent', () => {
   let component: StrategyComponent;
   let fixture: ComponentFixture<StrategyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StrategyComponent ]
     })
